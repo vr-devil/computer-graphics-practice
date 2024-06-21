5f104c1212cc6b8bccc8ccfd7c75c46a88d39a53
0xDEVIL <cnaicc@gmail.com>
2024-06-21 20:47:00 +0800

    实现绘制三角形。

---
+++ src/app.rs
@@ -0,0 +1,128 @@
+use std::fmt::Debug;
+use log::info;
+use wasm_bindgen::prelude::wasm_bindgen;
+use web_sys::console::info;
+use web_sys::HtmlCanvasElement;
+use winit::application::ApplicationHandler;
+use winit::dpi::PhysicalSize;
+use winit::error::EventLoopError;
+use winit::event::WindowEvent;
+use winit::event_loop::{ActiveEventLoop, ControlFlow};
+use winit::event_loop::EventLoop;
+use winit::platform::web;
+use winit::platform::web::EventLoopExtWebSys;
+use winit::window::{Window, WindowId};
+use yew::platform::spawn_local;
+use yew::prelude::*;
+
+use crate::graphics::WGPUState;
+
+#[wasm_bindgen]
+pub struct App {
+    wgpu_state: Option<WGPUState>,
+    canvas: NodeRef,
+}
+
+
+pub struct Handler {
+    canvas: HtmlCanvasElement,
+    wgpu_callback: Callback<WGPUState>,
+}
+
+pub enum AppMsg {
+    Initialize,
+    Initialized(WGPUState),
+    Redraw,
+    Nothing,
+}
+
+impl Component for App {
+    type Message = AppMsg;
+    type Properties = ();
+
+    fn create(ctx: &Context<Self>) -> Self {
+        info!("Creating App");
+        App {
+            wgpu_state: None,
+            canvas: NodeRef::default(),
+        }
+    }
+
+    fn update(&mut self, ctx: &Context<Self>, msg: Self::Message) -> bool {
+        match msg {
+            AppMsg::Initialize => {
+                info!("Initialize");
+
+                let wgpu_cb = ctx.link().callback(AppMsg::Initialized);
+                let handler = Handler {
+                    canvas: self.canvas.cast::<HtmlCanvasElement>().unwrap(),
+                    wgpu_callback: wgpu_cb,
+                };
+
+                let event_loop = EventLoop::new().unwrap();
+                event_loop.set_control_flow(ControlFlow::Wait);
+                event_loop.spawn_app(handler);
+
+            }
+            AppMsg::Initialized(state) => {
+                info!("Initialized");
+
+                self.wgpu_state = Some(state);
+                ctx.link().send_message(AppMsg::Redraw)
+            }
+            AppMsg::Redraw => {
+                info!("Redrawing");
+                self.wgpu_state.as_mut().unwrap().render().expect("TODO: panic message");
+            }
+            AppMsg::Nothing => {
+                info!("Nothing")
+            }
+        };
+
+        true
+    }
+
+    fn view(&self, _ctx: &Context<Self>) -> Html {
+        html! {
+            <div>
+                <h1>{"Hello, Computer Graphics"}</h1>
+                <canvas ref={self.canvas.clone()} id="canvas" width="800" height="600"></canvas>
+            </div>
+        }
+    }
+
+    fn rendered(&mut self, ctx: &Context<Self>, first_render: bool) {
+        info!("App rendered");
+        if first_render {
+            info!("App first render");
+            ctx.link().send_message(AppMsg::Initialize);
+        }
+    }
+}
+
+impl ApplicationHandler for Handler {
+    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
+        info!("App handler resumed");
+
+        use web::WindowAttributesExtWebSys;
+
+        let canvas = self.canvas.clone();
+        let window_attributes = Window::default_attributes()
+            .with_inner_size(PhysicalSize::new(canvas.width(), canvas.height()))
+            .with_canvas(Some(canvas));
+
+        let window = event_loop.create_window(window_attributes).unwrap();
+        let cb = self.wgpu_callback.clone();
+
+        spawn_local(async move {
+            cb.emit(WGPUState::new(window).await);
+        });
+
+    }
+
+    fn window_event(&mut self, _event_loop: &ActiveEventLoop, _window_id: WindowId, _event: WindowEvent) {}
+
+    fn suspended(&mut self, _event_loop: &ActiveEventLoop) {
+        info!("App handler suspended");
+    }
+}
---
+++ src/graphics.rs
@@ -0,0 +1,161 @@
+use std::borrow::Cow;
+use std::iter;
+
+use log::info;
+use web_sys::console::info;
+use wgpu::{Backends, BlendComponent, BlendState, Color, ColorTargetState, ColorWrites, CommandEncoderDescriptor, Device, DeviceDescriptor, Face, Features, FragmentState, FrontFace, Instance, InstanceDescriptor, Limits, LoadOp, MultisampleState, Operations, PipelineLayoutDescriptor, PolygonMode, PowerPreference, PrimitiveState, PrimitiveTopology, Queue, RenderPassColorAttachment, RenderPassDescriptor, RenderPipeline, RenderPipelineDescriptor, RequestAdapterOptions, ShaderModuleDescriptor, ShaderSource, StoreOp, Surface, SurfaceConfiguration, SurfaceError, TextureUsages, TextureViewDescriptor, VertexState};
+use winit::dpi::PhysicalSize;
+use winit::window::Window;
+
+pub struct WGPUState {
+    pub surface: Surface<'static>,
+    pub device: Device,
+    pub queue: Queue,
+    pub config: SurfaceConfiguration,
+    pub size: PhysicalSize<u32>,
+    pub render_pipeline: RenderPipeline,
+}
+
+impl WGPUState {
+    pub async fn new(window: Window) -> Self {
+        let mut size = window.inner_size();
+        size.width = size.width.max(500);
+        size.height = size.height.max(500);
+        info!("{:?}", size);
+
+        let instance = Instance::default();
+        info!("Created WGPU instance");
+
+        let surface = instance.create_surface(window).unwrap();
+        info!("Created WGPU surface");
+
+        let adapter = instance
+            .request_adapter(&RequestAdapterOptions {
+                power_preference: PowerPreference::default(),
+                force_fallback_adapter: false,
+                compatible_surface: Some(&surface),
+            })
+            .await
+            .unwrap();
+        info!("Created adapter");
+
+        let (device, queue) = adapter
+            .request_device(
+                &DeviceDescriptor {
+                    label: None,
+                    required_features: Features::empty(),
+                    required_limits: Limits::downlevel_webgl2_defaults()
+                        .using_resolution(adapter.limits()),
+                },
+                None,
+            )
+            .await
+            .unwrap();
+        info!("Created device and queue");
+
+        let shader = device.create_shader_module(ShaderModuleDescriptor {
+            label: Some("Shader"),
+            source: ShaderSource::Wgsl(Cow::Borrowed(include_str!("shader.wgsl"))),
+        });
+        info!("Created shader");
+
+        let pipeline_layout =
+            device.create_pipeline_layout(&PipelineLayoutDescriptor {
+                label: Some("Render Pipeline Layout"),
+                bind_group_layouts: &[],
+                push_constant_ranges: &[],
+            });
+        info!("Created pipeline layout");
+
+        let swapchain_capabilities = surface.get_capabilities(&adapter);
+        let swapchain_format = swapchain_capabilities.formats[0];
+
+        let render_pipeline = device
+            .create_render_pipeline(&RenderPipelineDescriptor {
+                label: Some("Render Pipeline"),
+                layout: Some(&pipeline_layout),
+                vertex: VertexState {
+                    module: &shader,
+                    entry_point: "vs_main",
+                    buffers: &[],
+                    compilation_options: Default::default(),
+                },
+                fragment: Some(FragmentState {
+                    module: &shader,
+                    entry_point: "fs_main",
+                    compilation_options: Default::default(),
+                    targets: &[Some(swapchain_format.into())],
+                }),
+                primitive: PrimitiveState::default(),
+                depth_stencil: None,
+                multisample: MultisampleState::default(),
+                multiview: None,
+            });
+        info!("Created render pipeline");
+
+        let mut config = surface
+            .get_default_config(&adapter, size.width, size.height)
+            .unwrap();
+        surface.configure(&device, &config);
+        info!("Created config");
+
+        Self {
+            surface,
+            device,
+            queue,
+            config,
+            size,
+            render_pipeline,
+        }
+    }
+
+    pub fn resize(&mut self, new_size: PhysicalSize<u32>) {
+        if new_size.width > 0 && new_size.height > 0 {
+            self.size = new_size;
+            self.config.width = new_size.width;
+            self.config.height = new_size.height;
+            self.surface.configure(&self.device, &self.config);
+        }
+    }
+
+    pub fn render(&mut self) -> Result<(), SurfaceError> {
+        info!("Rendering");
+        let frame = self.surface
+            .get_current_texture()
+            .expect("Failed to acquire next swap chain texture");
+        let view = frame
+            .texture
+            .create_view(&TextureViewDescriptor::default());
+
+        let mut encoder = self.device
+            .create_command_encoder(&CommandEncoderDescriptor {
+                label: Some("Render Encoder"),
+            });
+
+        {
+            let mut render_pass = encoder
+                .begin_render_pass(&RenderPassDescriptor {
+                    label: Some("Render Pass"),
+                    color_attachments: &[Some(RenderPassColorAttachment {
+                        view: &view,
+                        resolve_target: None,
+                        ops: Operations {
+                            load: LoadOp::Clear(Color::GREEN),
+                            store: StoreOp::Store,
+                        },
+                    })],
+                    depth_stencil_attachment: None,
+                    occlusion_query_set: None,
+                    timestamp_writes: None,
+                });
+
+            render_pass.set_pipeline(&self.render_pipeline);
+            render_pass.draw(0..3, 0..1);
+        }
+
+        self.queue.submit(Some(encoder.finish()));
+        frame.present();
+
+        Ok(())
+    }
+}

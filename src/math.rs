d3aeb0f2b092bd6bd7e87a3f6c0070c74c076f7e
0xDEVIL <cnaicc@gmail.com>
2025-03-26 20:51:48 +0800

    实现向量模型，以及向量的加、减、点积、叉积和负运算。

---
+++ src/math.rs
@@ -0,0 +1,148 @@
+use std::ops::{Add, Mul, Neg, Sub};
+
+
+#[derive(PartialOrd, PartialEq)]
+#[derive(Debug)]
+pub struct Vector3 {
+    x: f32,
+    y: f32,
+    z: f32,
+}
+
+impl Vector3 {
+    pub fn dot(&self, rhs: &Self) -> f32 {
+        self.x * rhs.x + self.y * rhs.y + self.z * rhs.z
+    }
+
+    pub fn cross(&self, rhs: &Self) -> Self {
+        Self {
+            x: self.y * rhs.z - self.z * rhs.y,
+            y: self.z * rhs.x - self.x * rhs.z,
+            z: self.x * rhs.y - self.y * rhs.x
+        }
+    }
+}
+
+impl Mul<&Vector3> for f32 {
+    type Output = Vector3;
+
+    fn mul(self, rhs: &Vector3) -> Self::Output {
+        Self::Output {
+            x: self * rhs.x,
+            y: self * rhs.y,
+            z: self * rhs.z,
+        }
+    }
+}
+
+impl Mul<f32> for &Vector3 {
+    type Output = Vector3;
+
+    fn mul(self, rhs: f32) -> Self::Output {
+        Self::Output {
+            x: self.x * rhs,
+            y: self.y * rhs,
+            z: self.z * rhs,
+        }
+    }
+}
+
+impl Neg for &Vector3 {
+    type Output = Vector3;
+
+    fn neg(self) -> Self::Output {
+        Self::Output {
+            x: -self.x,
+            y: -self.y,
+            z: -self.z,
+        }
+    }
+}
+
+impl Add<&Vector3> for &Vector3 {
+    type Output = Vector3;
+
+    fn add(self, rhs: &Vector3) -> Self::Output {
+        Vector3 {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+            z: self.z + rhs.z,
+        }
+    }
+}
+
+impl Sub<&Vector3> for &Vector3 {
+    type Output = Vector3;
+
+    fn sub(self, rhs: &Vector3) -> Self::Output {
+        Self::Output {
+            x: self.x - rhs.x,
+            y: self.y - rhs.y,
+            z: self.z - rhs.z,
+        }
+    }
+}
+
+impl Mul<&Vector3> for &Vector3 {
+    type Output = f32;
+
+    fn mul(self, rhs: &Vector3) -> Self::Output {
+        self.dot(rhs)
+    }
+}
+
+mod test_vector3 {
+    use crate::math::Vector3;
+
+    #[test]
+    fn test_add() {
+        let a = Vector3 { x: 1., y: 2., z: 3. };
+        let b = Vector3 { x: 1., y: 2., z: 3. };
+
+        assert_eq!(Vector3 { x: 2., y: 4., z: 6. }, &a + &b);
+        assert_eq!(Vector3 { x: 2., y: 4., z: 6. }, &b + &a);
+        assert_eq!(Vector3 { x: 2., y: 4., z: 6. }, &b + &a);
+    }
+
+    #[test]
+    fn test_sub() {
+        let a = Vector3 { x: 1., y: 2., z: 3. };
+        let b = Vector3 { x: 3., y: 2., z: 1. };
+        let b = Vector3 { x: 3., y: 2., z: 1. };
+
+        assert_eq!(Vector3 { x: -2., y: 0., z: 2. }, &a - &b);
+        assert_eq!(Vector3 { x: -2., y: 0., z: 2. }, &a - &b);
+        assert_eq!(Vector3 { x: 2., y: 0., z: -2. }, &b - &a);
+        assert_eq!(Vector3 { x: 2., y: 0., z: -2. }, &b - &a);
+    }
+
+    #[test]
+    fn test_dot() {
+        let a = Vector3 {x: 1., y: 2., z: 3.};
+        let b = Vector3 {x: 1., y: 2., z: 3.};
+
+        assert_eq!(14., a.dot(&b));
+        assert_eq!(14., b.dot(&a));
+        assert_eq!(14., &a * &b);
+        assert_eq!(14., &b * &a);
+    }
+
+    #[test]
+    fn test_cross() {
+        let x = super::X;
+        let y = super::Y;
+        let z = super::Z;
+
+        assert_eq!(x, y.cross(&z));
+        assert_eq!(y, z.cross(&x));
+        assert_eq!(z, x.cross(&y));
+
+        assert_eq!(-&x, z.cross(&y));
+        assert_eq!(-&y, x.cross(&z));
+        assert_eq!(-&z, y.cross(&x));
+    }
+}
+
+const X: Vector3 = Vector3 { x: 1., y: 0., z: 0. };
+const Y: Vector3 = Vector3 { x: 0., y: 1., z: 0. };
+const Z: Vector3 = Vector3 { x: 0., y: 0., z: 1. };

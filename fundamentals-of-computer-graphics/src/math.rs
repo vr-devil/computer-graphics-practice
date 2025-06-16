eeaeb8ba36aa7b5bad78d22be155d2dc58ccedb4
VR DEVIL <cnaicc@gmail.com>
2025-06-16 15:02:19 +0800

    编写寻找光线与三角形的交点的测试用例。

--- fundamentals-of-computer-graphics/src/math.rs
+++ fundamentals-of-computer-graphics/src/math.rs
@@ -4,15 +4,19 @@ use std::ops::{Add, Div, Mul, Neg, Sub};
 #[repr(C)]
 #[derive(PartialOrd, PartialEq, Debug, Pod, Zeroable, Copy, Clone)]
 pub struct Vector3 {
-    x: f32,
-    y: f32,
-    z: f32,
+    pub x: f32,
+    pub y: f32,
+    pub z: f32,
 }
 
 impl Vector3 {
     pub fn new(x: f32, y: f32, z: f32) -> Self {
         Self {x, y, z}
     }
+}
+
+pub fn vec3(x: f32, y: f32, z: f32) -> Vector3 {
+    Vector3::new(x, y, z)
 }
 
 impl Div<f32> for &Vector3 {
--- fundamentals-of-computer-graphics/src/raytracing.rs
+++ fundamentals-of-computer-graphics/src/raytracing.rs
@@ -1,12 +1,18 @@
-use crate::math::Vector3;
+use crate::math::{Vector3};
 
 struct Ray {
     pub e: Vector3,
     pub d: Vector3,
 }
 
+impl Ray {
+    pub fn point(&self, t: f32) -> Vector3 {
+        &self.e + &(t * &self.d)
+    }
+}
+
 #[test]
-fn test_intersect_sphere() {
+fn test_ray_sphere_intersection() {
     // sphere
     let r = 1.0f32;
     let o = Vector3::new(0.0, 0.0, 0.0);
@@ -24,13 +30,13 @@ fn test_intersect_sphere() {
     println!("a:{}, b:{}, c:{}, discriminant:{}", a, b, c, discriminant);
     assert!(discriminant > 0.);
 
-    let t1 = (-b-discriminant.sqrt()) / (2. * a);
-    let t2 = (-b+discriminant.sqrt()) / (2. * a);
+    let t1 = (-b - discriminant.sqrt()) / (2. * a);
+    let t2 = (-b + discriminant.sqrt()) / (2. * a);
 
     println!("t1:{}, t2: {}", t1, t2);
 
-    let p1 = &e+&(t1 * &d);
-    let p2 = &e+&(t2 * &d);
+    let p1 = &e + &(t1 * &d);
+    let p2 = &e + &(t2 * &d);
     println!("p1:{:?} p2:{:?}", p1, p2);
 
     let p1 = p1.norm();
@@ -39,4 +45,54 @@ fn test_intersect_sphere() {
 
     assert_eq!(p1.round(), 1., "p1");
     assert_eq!(p2.round(), 1., "p2");
+}
+
+#[test]
+fn test_ray_triangle_intersection() {
+
+    use crate::math::vec3;
+
+    // ray
+    let ray = Ray {
+        e: vec3(1., 1., 1.),
+        d: vec3(-1., -1., -1.),
+    };
+
+    // triangle
+    let a = vec3(1., 0., 0.);
+    let b = vec3(0., 1., 0.);
+    let c = vec3(0., 0., 1.);
+
+    {
+        let ((a, b, c), (d, e, f), (g, h, i), (j, k, l)) = (
+            (a.x - b.x, a.y - b.y, a.z - b.z),
+            (a.x - c.x, a.y - c.y, a.z - c.z),
+            (ray.d.x, ray.d.y, ray.d.z),
+            (a.x - ray.e.x, a.y - ray.e.y, a.z - ray.e.z),
+        );
+
+        let m = a * (e * i - h * f) + b * (g * f - d * i) + c * (d * h - e * g);
+        let beta = (j * (e * i - h * f) + k * (g * f - d * i) + l * (d * h - e * g)) / m;
+        let gamma = (i * (a * k - j * b) + h * (j * c - a * l) + g * (b * l - k * c)) / m;
+        let alpha = 1. - beta - gamma;
+        let t = -(f * (a * k - j * b) + e * (j * c - a * l) + d * (b * l - k * c)) / m;
+        let p = ray.point(t);
+
+        println!("alpha:{alpha}, beta:{beta}, gamma:{gamma}, t:{t}");
+        println!("p:{:?}", p);
+
+        let (t0, t1) = (0., 1.);
+
+        let hit = if t < t0 || t > t1 {
+            false
+        } else if gamma < 0. || gamma > 1. {
+            false
+        } else if beta < 0. || beta > 1. - gamma {
+            false
+        } else {
+            true
+        };
+
+        assert_eq!(hit, true);
+    }
 }
